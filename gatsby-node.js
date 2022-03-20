b60e6c6bc3f926d2e85671986274d7e91b785194
J J Walwyn <joshwalwyn@economist.com>
2022-03-20 22:44:07 +0000

    Add vimspector debugging support

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,11 @@
 const path = require('path');
 
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    devtool: 'eval-source-map',
+  });
+};
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
