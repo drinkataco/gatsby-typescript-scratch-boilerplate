f992a5b66d5e5b128b127ee49d0ae9bf0096f4fa
J J Walwyn <joshwalwyn@economist.com>
2022-04-15 10:38:43 +0100

    Fix ActiveClass link

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -13,11 +13,11 @@ function Navigation() {
       name: 'Home',
     },
     {
-      location: '/about',
+      location: '/about/',
       name: 'About',
     },
     {
-      location: '/video',
+      location: '/video/',
       name: 'Video',
     },
   ];
